2aaa0c0eb10d3b60f42a95daa0baebde25cb53db
tokinirr <tnirina32@gmail.com>
2026-09-03 20:51:50 +0300

    feat(utils) function that split paragraphs

--- src/rag/chunking/utils.py
+++ src/rag/chunking/utils.py
@@ -1,4 +1,5 @@
 import re
+from typing import Any
 
 
 def clean_text(text: str) -> str:
@@ -12,3 +13,13 @@ def clean_text(text: str) -> str:
     text = re.sub(r"\n{3,}", "\n\n", text)
 
     return text.strip()
+
+
+def split_paragraphs(text: str) -> list[str]:
+    pargraphs: list[str | Any] = re.split(r"\n\s*\n", text)
+
+    return [
+        paragraph.strip()
+        for paragraph in pargraphs
+        if paragraph.strip()
+    ]
