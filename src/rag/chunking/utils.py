265959d66d7a94481f959dd719f04747bd353a9f
tokinirr <tnirina32@gmail.com>
2026-09-03 21:02:16 +0300

    feat(utils) add overlap function

--- src/rag/chunking/utils.py
+++ src/rag/chunking/utils.py
@@ -43,3 +43,33 @@ def get_last_words(text: str, n: int) -> str:
     if not words:
         return ""
     return " ".join(words[-n:])
+
+
+def add_overlap(
+    chunks: list[str],
+    overlap_tokens: int
+) -> list[str]:
+
+    if not chunks:
+        return []
+
+    result: list[str] = [chunks[0]]
+
+    overlap_words: int = max(
+        1,
+        int(overlap_tokens / 1.3),
+    )
+
+    for current_index in range(1, len(chunks)):
+        previous_chunk: str = chunks[current_index - 1]
+        overlap: str = get_last_words(
+            previous_chunk,
+            overlap_words,
+        )
+
+        current_chunk = chunks[current_index]
+
+        result.append(
+            f"{overlap}\n\n{current_chunk}"
+        )
+    return result
