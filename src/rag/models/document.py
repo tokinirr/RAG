d2967aee688e023148b1341919457e057dffbdb6
tokinirr <tnirina32@gmail.com>
2026-09-03 20:20:04 +0300

    feat(document) make models for chunk

--- src/rag/models/document.py
+++ src/rag/models/document.py
@@ -11,3 +11,17 @@ class Document:
     source: str
     file_type: str
     page: int | None = None
+
+
+@dataclass
+class Chunk:
+    contetn: str
+    source: str
+    file_type: str
+
+    chunk_id: int
+
+    section: str | None = None
+
+    page_start: int | None = None
+    page_end: int | None = None
