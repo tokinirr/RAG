aba483364c3e9e61ac5b13a47f45fd2412f0df54
tokinirr <tnirina32@gmail.com>
2026-09-03 20:46:35 +0300

    feat(utils) create function clen text

---
+++ src/rag/chunking/utils.py
@@ -0,0 +1,14 @@
+import re
+
+
+def clean_text(text: str) -> str:
+    text = text.replace("\r\n", "\n")
+    text = text.replace("\r", "\n")
+
+    text = re.sub(r"[ \t]+", " ", text)
+
+    text = re.sub(r" *\n *", "\n", text)
+
+    text = re.sub(r"\n{3,}", "\n\n", text)
+
+    return text.strip()
